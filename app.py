2395fb3794af82a461fee6628b15ba2871096d79
Niyiiyanda <stopniyi@gmail.com>
2026-09-18 18:37:38 +0000

    Fix spacing on line 234

--- app.py
+++ app.py
@@ -231,7 +231,8 @@ def render_player_cards(df):
     </div>
 </div>
 """
-st.markdown(player_card_html, unsafe_allow_html=True)with tab1:
+st.markdown(player_card_html, unsafe_allow_html=True)
+with tab1:
     render_player_cards(home_players_live)
 with tab2:
     render_player_cards(away_players_live)
